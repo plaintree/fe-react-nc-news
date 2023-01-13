f0647cceda719805e5b0c5ff7ee8d76e313ff6fa
plaintree <cheukhei04@hotmail.com>
2023-01-13 14:03:13 +0000

    add mobile menu button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
+import dogImg from "../assets/home-dog-reading.jpg";
+
 const Home = () => {
   return (
     <section className="home__container">
       <h1>Here, have a nice Dog!</h1>
-      <img
-        src="https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2836&q=80"
-        alt="dog-reading"
-      />
+      <img src={dogImg} alt="dog-reading" />
       <h5>Click the links above to get access to different API</h5>
       <h5>
         Go to the User page and select a user to create or delete comments
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,20 +1,53 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
+import { ImCross } from "react-icons/im";
 import logo from "../../assets/nc-logo.png";
 const Navbar = () => {
-  return (
-    <div className="nav__container">
-      <header className="nav__container__logo">
-        <img src={logo} alt="ryan-ncn" />
-        <h1>Welcome to Ryan's NC News</h1>
-      </header>
+  const [showMenu, setShowMenu] = useState(false);
 
-      <nav className="nav__container__links">
-        <Link to="/">Home</Link>
-        <Link to="/articles">Articles</Link>
-        <Link to="/topics">Topics</Link>
-        <Link to="/users">Users</Link>
-      </nav>
-    </div>
+  return (
+    <>
+      <div className="nav__container">
+        <header className="nav__container__logo">
+          <img src={logo} alt="ryan-ncn" />
+          <h1>Ryan's NC News</h1>
+        </header>
+
+        <nav className="nav__container__links">
+          <Link to="/">Home</Link>
+          <Link to="/articles">Articles</Link>
+          <Link to="/topics">Topics</Link>
+          <Link to="/users">Users</Link>
+        </nav>
+
+        {showMenu ? (
+          <ImCross
+            onClick={() => setShowMenu(false)}
+            className="mobile__menu"
+          />
+        ) : (
+          <HiMenu onClick={() => setShowMenu(true)} className="mobile__menu" />
+        )}
+      </div>
+      {showMenu && (
+        <nav className="nav__mobile">
+          <Link to="/" onClick={() => setShowMenu(false)}>
+            Home
+          </Link>
+          <Link to="/articles" onClick={() => setShowMenu(false)}>
+            Articles
+          </Link>
+          <Link to="/topics" onClick={() => setShowMenu(false)}>
+            Topics
+          </Link>
+          <Link to="/users" onClick={() => setShowMenu(false)}>
+            Users
+          </Link>
+          <p>&copy; Plaintree Production. All right reserved.</p>
+        </nav>
+      )}
+    </>
   );
 };
 export default Navbar;
